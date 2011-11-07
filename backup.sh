efbceafc3a31c4d3a1c3c642e8d3e6a1b557851d
Daisuke Homma <daisuke.homma+github@gmail.com>
2011-11-07 22:24:20 +0900

    changes for uploading

--- backup.sh
+++ backup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-BKDIR=${HOME}/Dropbox/Anima/History/`date '+%Y-%m%d-%H%M.%S'`
-export BKDIR
+#BKDIR=${HOME}/Dropbox/Anima/History/`date '+%Y-%m%d-%H%M.%S'`
+#export BKDIR
+#cp -Rp . ${BKDIR}
 
-cp -Rp . ${BKDIR}
-
+git add .
+git commit -a
+git push origin master
